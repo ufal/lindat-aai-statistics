c02155f2076e8c9508b544cf969219c6bdf19dbb
Michal Sedlak <sedlakmichal@gmail.com>
2015-08-05 23:32:06 +0200

    One more fix to the build script for Travis

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+git_user="Michal Sedlák"
+git_email="sedlakmichal@gmail.com"
+
 rm -rf dist
 mkdir -p dist/tabs
 cp -r public/tabs/*.html dist/tabs
@@ -10,7 +13,10 @@ cp bower_components/angular-ui-grid/*.{ttf,woff,eot,svg} dist
 
 if [ -n "$TRAVIS" ]; then
 
-git add -f dist
-git commit -m "Release from Travis build #$TRAVIS_BUILD_NUMBER"
+  git config user.name "$git_user"
+  git config user.email "$git_email"
+
+  git add -f dist
+  git commit -m "Release from Travis build #$TRAVIS_BUILD_NUMBER"
 
 fi
