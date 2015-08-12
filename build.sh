741131560ed0b501349aedee3f46853eebcb1803
Michal Sedlák <sedlakmichal@gmail.com>
2015-08-12 15:30:29 +0200

    Fix build - add missing images

--- build.sh
+++ build.sh
@@ -8,9 +8,12 @@ git_email="sedlakmichal@gmail.com"
 rm -rf dist
 mkdir -p dist/tabs
 mkdir -p dist/fonts
+mkdir -p dist/images
+
 cp -r public/tabs/*.html dist/tabs
 cp bower_components/angular-ui-grid/*.{ttf,woff,eot,svg} dist
 cp bower_components/bootstrap/fonts/* dist/fonts
+cp -r bower_components/lindat-common/dist/public/{images,fonts} dist
 ./node_modules/.bin/useref public/index.html dist/index.html --css uglify --js uglify
 
 if [ -n "$TRAVIS" ]; then
